b2a3af8cbbfab596809299b7cef853f136f7e91f
Imran Remtulla <iremtulla15@gmail.com>
2021-12-30 17:26:41 -0500

    Gave up and went with basic option for dark mode
    No manual toggle (follows system only)
    
    No customization (some items have low contrast,
    can't easily fix due to lack of SCSS @extend inside media query)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frontload';
+
+  @HostBinding('class') className = ''
+
+  ngOnInit(): void {
+      document.querySelector('html')?.classList.add('dark-mode')
+  }
 }
